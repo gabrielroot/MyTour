7110a291d0744c8a89dfd65edae3835a9166ce3f
gabrielroot <gabrielfer.s88@gmail.com>
2024-07-30 21:50:21 -0300

    feat(Structure): Initialize project structure.

---
+++ src/ExcursionBundle/MyTourExcursionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MyTour\ExcursionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class MyTourExcursionBundle extends Bundle
+{
+}
---
+++ src/UserBundle/MyTourUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MyTour\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class MyTourUserBundle extends Bundle
+{
+}
