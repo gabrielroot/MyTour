cfa76a6931ede03f20d842d1a67d439d64457f98
gabrielroot <gabrielfer.s88@gmail.com>
2024-10-12 16:20:46 -0300

    feat(AssetMapper): Use AssetMapper to install third party libs.

---
+++ importmap.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Returns the importmap for this application.
+ *
+ * - "path" is a path inside the asset mapper system. Use the
+ *     "debug:asset-map" command to see the full list of paths.
+ *
+ * - "entrypoint" (JavaScript only) set to true for any module that will
+ *     be used as an "entrypoint" (and passed to the importmap() Twig function).
+ *
+ * The "importmap:require" command can be used to add new entries to this file.
+ */
+return [
+    'app' => [
+        'path' => './assets/app.js',
+        'entrypoint' => true,
+    ],
+    'app_css' => [
+        'path' => './assets/styles/app.css',
+        'type' => 'css',
+    ],
+    'flatpickr' => [
+        'version' => '4.6.13',
+    ],
+    'flatpickr/dist/l10n/pt.js' => [
+        'version' => '4.6.13',
+    ],
+    'flatpickr/dist/flatpickr.min.css' => [
+        'version' => '4.6.13',
+        'type' => 'css',
+    ],
+    '@fortawesome/fontawesome-free/css/all.min.css' => [
+        'version' => '6.6.0',
+        'type' => 'css'
+    ],
+    '@fortawesome/fontawesome-free/js/all.min.js' => [
+        'version' => '6.6.0',
+    ],
+    '@fortawesome/fontawesome-free' => [
+        'version' => '6.6.0',
+    ],
+    '@fortawesome/fontawesome-free/css/fontawesome.min.css' => [
+        'version' => '6.6.0',
+        'type' => 'css',
+    ],
+];
