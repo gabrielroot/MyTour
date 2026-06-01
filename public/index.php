5bf750a6177c1760b0c55079e3633d34e683b577
gabrielroot <gabrielfer.s88@gmail.com>
2026-05-31 21:40:33 -0300

    fix(index.php): Add X-Replica header to output hostname for better debugging

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ use MyTour\Kernel;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
+header('X-Replica: ' . gethostname());
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
